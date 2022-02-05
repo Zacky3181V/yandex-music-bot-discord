b726b096eca634170d9bfb6a76402887d6626c5a
Zacky3181V <93855417+Zacky3181V@users.noreply.github.com>
2022-02-05 20:18:40 +0600

    Add files via upload

---
+++ music.py
@@ -0,0 +1,11 @@
+from yandex_music import Client
+import authorise as aut
+import config as c
+
+client = Client.from_token(aut.OAUTH)
+
+
+#url = 'https://music.yandex.ru/album/7257985/track/51842422'
+
+
+
